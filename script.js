018d0ce46f5eedeb8bdbfc6fd062df589bbbb627
calvinatian <48587962+calvinatian@users.noreply.github.com>
2021-11-24 00:18:12 -0500

    Fix dining halls

--- script.js
+++ script.js
@@ -2,23 +2,26 @@ let dining_halls_all = new Set();
 
 dining_halls_all.add("Bridgeview Market");
 dining_halls_all.add("Café 370");
-dining_halls_all.add("Market at Third North");
-dining_halls_all.add("Market at Palladium");
-dining_halls_all.add("The Marketplace at Kimmel");
-dining_halls_all.add("BRKFST & CO at UHALL");
+dining_halls_all.add("BRKFST & CO at UHALL (U-Hall Commons Cafe)");
 dining_halls_all.add("18 Below");
 dining_halls_all.add("Dunkin'");
-dining_halls_all.add("Jasper Kane Cafe");
 dining_halls_all.add("Kosher Eatery");
+dining_halls_all.add("The Marketplace at Kimmel");
+dining_halls_all.add("Market at Palladium");
 dining_halls_all.add("Market at Lipton");
 dining_halls_all.add("Market at Sidestein");
 dining_halls_all.add("Market at UHall");
+dining_halls_all.add("Market at Third North");
+dining_halls_all.add("Jasper Kane Cafe");
+dining_halls_all.add("Upstein Smoothie Lab ");
+dining_halls_all.add("NYU EATS at Third North");
 dining_halls_all.add("NYU EATS at Downstein");
 dining_halls_all.add("NYU EATS at Lipton");
 dining_halls_all.add("Palladium");
+dining_halls_all.add("Palladium Brunch");
 dining_halls_all.add("Peet's Coffee");
 dining_halls_all.add("Starbucks");
-dining_halls_all.add("Upstein Daily Press");
+dining_halls_all.add("Cluckstein");
 dining_halls_all.add("Upstein The Egg Shoppe");
 
 window.onload = function (event) {
